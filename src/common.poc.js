7e8a85588c6baa404be2f1298eac23b8163a9185
Andreas Klöber <andreas.kloeber@gmail.com>
2017-01-05 22:58:06 +0100

    perf test for highland completed

--- src/common.poc.js
+++ src/common.poc.js
@@ -2,26 +2,34 @@
 
 const DATA = [12, 34, 56, 78, 90];
 
-exports.createIterable = () => ({
-  [Symbol.iterator]: function() {
-    return {
-      _data: DATA,
-      _count: DATA.length,
-      _idx: 0,
-      next: function() {
-        const done = this._idx === this._count;
-        const value = !done ? this._data[this._idx++] : undefined;
+exports.createIterable = opts => {
+  const cfg = Object.assign({
+    data: DATA,
+    quiet: false
+  }, opts);
 
-        console.log(done ? 'IT END' : `IT VALUE ${value}`);
+  return {
+    [Symbol.iterator]: function() {
+      return {
+        _data: cfg.data,
+        _count: cfg.data.length,
+        _tap: !cfg.quiet,
+        _idx: 0,
+        next: function() {
+          const done = this._idx === this._count;
+          const value = !done ? this._data[this._idx++] : undefined;
 
-        return {
-          value: value,
-          done: done
-        };
-      }
-    };
-  }
-});
+          this._tap && console.log(done ? 'IT END' : `IT VALUE ${value}`);
+
+          return {
+            value: value,
+            done: done
+          };
+        }
+      };
+    }
+  };
+};
 
 exports.DURATION_ASYNC_TASK = 1000;
 exports.CHUNK_SIZE = 2;
--- test/perf.test.js
+++ test/perf.test.js
@@ -6,48 +6,44 @@ const Benchmark = require('benchmark');
 const common = require('../src/common.poc');
 const _ = require('highland');
 
-const BENCH_OPTS = {
-  defer: true,
-  setup: function() {
-    console.log('SETUP', this);
-    var ITERABLE = common.createIterable();
-  },
-  teardown: function() {},
-  context: {
-    COUNT: 1000,
-    CHUNK_SIZE: 2,
-    DURATION_ASYNC_TASK: 0
-  }
-};
+const COUNT = 20000;
+const CHUNK_SIZE = 2000;
+const DURATION_ASYNC_TASK = 0;
+
+const DATA = [];
+for (let idx = 0; idx < COUNT; idx++) {
+  DATA[idx] = Math.floor(Math.random() * 1000);
+}
+
+const ITERABLE = common.createIterable({data: DATA, quiet: true});
 
 new Benchmark.Suite('Iteration over Array')
-.add('highland', function(deferred) {
-  console.log('TEST', this.benchmark.compiled);
-  console.log('TEST', this.benchmark.context);
-  console.log('TEST', typeof ITERABLE);
-
-  new Promise(function(resolve, reject) {
-    _(ITERABLE)
-      .batch(CHUNK_SIZE)
-      .flatMap(x => {
-        return _(new Promise(pResolve => {
-          setTimeout(() => {
-            pResolve(x);
-          }, DURATION_ASYNC_TASK);
-        }));
-      })
-      .last()
-      .toCallback((err, result) => {
-        if(err) {
-          reject(err);
-        } else {
-          resolve(result);
-        }
-      });
-  })
-  .then(() => deferred.resolve())
-  .catch(err => deferred.reject(err));
-}, BENCH_OPTS)
+.add('highland', {
+  fn: function(deferred) {
+    new Promise(function(resolve, reject) {
+      _(ITERABLE)
+        .batch(CHUNK_SIZE)
+        .flatMap(x => {
+          return _(new Promise(pResolve => {
+            setTimeout(() => {
+              pResolve(x);
+            }, DURATION_ASYNC_TASK);
+          }));
+        })
+        .last()
+        .toCallback((err, result) => {
+          if(err) {
+            reject(err);
+          } else {
+            resolve(result);
+          }
+        });
+    })
+    .then(() => deferred.resolve())
+    .catch(err => deferred.reject(err));
+  },
+  defer: true
+})
 .on('error', function(event) {
   console.error('ERROR:', event.target.error, event.target.error.stack);
 })
