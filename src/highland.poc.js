bc744b5b9a1f7f0a2be3f2589b3cb5fda1f565d2
Andreas Klöber <andreas.kloeber@gmail.com>
2017-01-09 20:45:04 +0100

    example for error handling added to highland POC

--- src/highland.poc.js
+++ src/highland.poc.js
@@ -7,11 +7,12 @@ new Promise(function(resolve, reject) {
   _(common.createIterable())
     .batch(common.CHUNK_SIZE)
     .flatMap(x => {
-      return _(new Promise(pResolve => {
+      return _(new Promise((pResolve, pReject) => {
         console.log('ASYNC START', x);
         setTimeout(() => {
           console.log('ASYNC COMPLETE', x);
           pResolve(x);
+          //pReject(new Error('some error'));
         }, common.DURATION_ASYNC_TASK);
       }));
     })
@@ -25,4 +26,5 @@ new Promise(function(resolve, reject) {
       }
     });
 })
-.then(() => console.log('DONE'));
+.then(() => console.log('DONE'))
+.catch(err => console.log('ERROR', err));
